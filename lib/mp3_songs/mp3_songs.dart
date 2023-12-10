ef62839c7b9147d71a6b1718befd937954a58e45
chandu <chandu.christ@gmail.com>
2023-12-11 00:00:37 +0530

    mp3 sons add-8

--- lib/mp3_songs/mp3_songs.dart
+++ lib/mp3_songs/mp3_songs.dart
@@ -8,6 +8,18 @@ class Mp3Songs {
 List mp3songs = [
   Mp3Songs("అదిగో ప్రియుని కేక", "సత్య స్వరూపుడా నా యేసయ్య",
       'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/2-ADHIGO-PRIYUNI-KEKA-WAV.mp3'),
-  Mp3Songs("ఏ యోగ్యత", "దీవించు మా తండ్రి",
-      'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3')
+  Mp3Songs("వెండి బంగారములు", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/4-VENDI-BANGARALU.mp3'),
+  Mp3Songs("రాజువైన నా దేవా", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/2-ADHIGO-PRIYUNI-KEKA-WAV.mp3'),
+  Mp3Songs("యేసు రక్తమే", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/6-yesu-rakthame-song.mp3'),
+  Mp3Songs("ప్రేమకే స్వాగతం", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/3-PREMAKY-SWAGATHAM.mp3'),
+  Mp3Songs("నా నీతికి ఆధారమా", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/1-NA-NITHIKI-ADHARAMA-WAV.mp3'),
+  Mp3Songs("కావాలి కావాలి", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/7-KAAVALI-KAAVALI-WAV.mp3'),
+  Mp3Songs("సర్వ శక్తిగల దేవా", "సత్య స్వరూపుడా నా యేసయ్య",
+      'https://thandrisannidhiministries.org/wp-content/uploads/2023/12/8-SARVA-SHAKTHI-GALA-DEVA.mp3'),
 ];
